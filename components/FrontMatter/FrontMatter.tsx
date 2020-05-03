5259e6cc3d7c0b4c27db1bb68e1a40681b5d184f
Matthew Seccafien <matt.seccafien@shopify.com>
2020-05-03 05:31:12 -0400

    test deployment again

--- components/FrontMatter/FrontMatter.tsx
+++ components/FrontMatter/FrontMatter.tsx
@@ -15,19 +15,6 @@ const StyledFrontMatter = styled.div<Partial<Props>>`
   ${respondTo.md`
     max-width: 50%;
   `}
-
-  &::before {
-    position: absolute;
-    top: 0;
-    left: 0;
-    content: '';
-    display: block;
-    width: 200vw;
-    height: 200vw;
-    background: #00ffc412;
-    z-index: -1;
-    transform: rotate(-45deg) translate(0, -95%);
-  }
 `;
 
 export default function FrontMatter({title}: Props) {
