c7fc19364adaf271f1d9128ac4c286a99ddbb602
Matthew Seccafien <matt.seccafien@shopify.com>
2020-05-23 11:25:06 -0400

    add env variables

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -38,7 +38,7 @@ const StyledLogo = styled.div<Partial<Props>>`
 `;
 
 export default function Logo() {
-  console.log('title:', title);
+  console.log('title:', title, process.env.SITE_NAME);
   return (
     <StyledLogo>
       <A href="/">
